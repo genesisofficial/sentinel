bd67042d7866a34cb6af24f394d9609419ddaea6
Nigho <nico.lucciola@googlemail.com>
2018-04-25 13:06:45 +0200

    Updated calls to daemon
    Since bitcoin core 0.16, "getinfo" won't work anymore and was replaced
    by "getnetworkinfo", "getblockchaininfo" and "getwalletinfo".

--- lib/dashd.py
+++ lib/dashd.py
@@ -43,7 +43,7 @@ class DashDaemon():
 
     # common RPC convenience methods
     def is_testnet(self):
-        return self.rpc_command('getinfo')['testnet']
+        return (self.rpc_command('getblockchaininfo')['chain'] == "test")
 
     def get_masternodes(self):
         mnlist = self.rpc_command('masternodelist', 'full')
@@ -241,7 +241,7 @@ class DashDaemon():
 
     @property
     def has_sentinel_ping(self):
-        getinfo = self.rpc_command('getinfo')
+        getinfo = self.rpc_command('getnetworkinfo')
         return (getinfo['protocolversion'] >= config.min_dashd_proto_version_with_sentinel_ping)
 
     def ping(self):
